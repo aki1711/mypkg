76b58984f199234d02cda085c0b5b91b95a82c98
aki1711 <s21c1140gf@s.chibakoudai.jp>
2023-01-04 01:11:38 +0900

    Add a command

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -1,10 +1,10 @@
- import launch
- import launch.actions
- import launch.substitutions
- import launch_ros.actions
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
 
 
- def generate_launch_description():
+def generate_launch_description():
 
      talker = launch_ros.actions.Node(
          package='mypkg',
